7596d364fa03cbae9e654591ecbc766d23bdba80
JohnBucher <johnbucher289@gmail.com>
2019-09-22 15:05:01 -0600

    Pushed improvements to "Background" layer: Removing useless code, adding comments, assigning better names, etc.

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, AfterContentInit, ViewEncapsulation } from '@angular/core';
-import { trigger, state, style, animate, transition, keyframes, query, stagger } from '@angular/animations';
-import { delay } from 'q';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-background',
@@ -8,21 +6,12 @@ import { delay } from 'q';
   styleUrls: ['./background.component.css', '../app.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class BackgroundComponent implements OnInit, AfterContentInit {
+export class BackgroundComponent {
   backgroundBlurb: string = `Software Engineer specializing in Web Development and Web Service technologies. I graduated with a bachelor’s degree in
                               Computer Science from Shippensburg University and have plans to continue my education with a master’s degree in the very
                               near future. I am currently working for Lockheed Martin as an Asc Software Engineer in the Space department. I am currently
                               focused on learning the industry and developing as an early-career professional.`
 
   constructor() {
-
-  }
-
-  ngOnInit() {
-    
-  }
-
-  ngAfterContentInit() {
-    
   }
 }
