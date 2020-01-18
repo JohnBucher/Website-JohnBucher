3a914dc8308ca7f0b47f479a563e2450d581df19
JohnBucher <johnbucher289@gmail.com>
2020-01-17 20:54:50 -0700

    Pushed re-work of the skills component: Cut down on code, made it easier to maintain skills in the future, tweaked formatting, and fixed tsLint warnings

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -12,7 +12,10 @@ export class SkillsComponent implements AfterContentInit {
   frontEndSymbol = '</ >';
   frontEndText = 'Front End';
   backEndSymbol = '{ }';
-  backEndText = 'Back End';
+  backEndText = 'Back End / Other Languages';
+
+  // HTML
+  starHtml = '<i class="pi pi-star skill-year-star"></i>';
 
   // Dialog booleans
   frontEndDialog = false;
@@ -20,11 +23,114 @@ export class SkillsComponent implements AfterContentInit {
   industryStandardsDialog = false;
   otherSkillsDialog = false;
 
+  frontEndSkillMap = new Map([
+    ['Angular', '2'],
+    ['AngularJS', '2'],
+    ['HTML', '3'],
+    ['CSS', '3'],
+    ['Typescript', '3'],
+    ['Javascript', '2'],
+    ['Bootstrap', '3'],
+    ['jQuery', '2'],
+  ]);
+  backEndSkillMap = new Map([
+    ['C#', '3'],
+    ['Java', '7'],
+    ['C', '5'],
+    ['SQL', '3'],
+    ['ASP.NET', '1'],
+    ['Ruby On Rails', '1'],
+    ['Python', '1'],
+    ['Kotlin', '1'],
+    ['Spring', '1'],
+  ]);
+  industrySkillMap = new Map([
+    ['Agile', '2'],
+    ['Git', '4'],
+    ['VSCode', '2'],
+    ['IntelliJ', '2'],
+    ['Visual Studio', '3'],
+    ['Eclipse', '4'],
+    ['JSON', '3'],
+    ['XML', '1'],
+    ['Relational Databases', '3'],
+    ['SQL Server', '1'],
+  ]);
+  otherSkillList = ['Continuous Self-Learning', 'Microsoft Office Products'];
+
   constructor() {
   }
 
   ngAfterContentInit() {
     this.state = (this.state === 'small' ? 'large' : 'small');
+    this.populateSkillLabels();
+  }
+
+  populateSkillLabels() {
+    const frontEndElementHTML = document.getElementById('frontEndSkillList');
+    const backEndElementHTML = document.getElementById('backEndSkillList');
+    const industryElementHTML = document.getElementById('industrySkillList');
+    const otherElementHTML = document.getElementById('otherSkillList');
+
+    // Front End
+    this.frontEndSkillMap.forEach((value: string, key: string) => {
+      frontEndElementHTML.innerHTML += (key + ', ');
+    });
+    frontEndElementHTML.innerHTML = frontEndElementHTML.innerHTML.substr(0, frontEndElementHTML.innerHTML.length - 2);
+
+    // Back End
+    this.backEndSkillMap.forEach((value: string, key: string) => {
+      backEndElementHTML.innerHTML += (key + ', ');
+    });
+    backEndElementHTML.innerHTML = backEndElementHTML.innerHTML.substr(0, backEndElementHTML.innerHTML.length - 2);
+
+    // Industry
+    this.industrySkillMap.forEach((value: string, key: string) => {
+      industryElementHTML.innerHTML += (key + ', ');
+    });
+    industryElementHTML.innerHTML = industryElementHTML.innerHTML.substr(0, industryElementHTML.innerHTML.length - 2);
+
+    // Other
+    this.otherSkillList.forEach((item: string) => {
+      otherElementHTML.innerHTML += (item + ', ');
+    });
+    otherElementHTML.innerHTML = otherElementHTML.innerHTML.substr(0, otherElementHTML.innerHTML.length - 2);
+  }
+
+  populateFrontEndDialog() {
+    this.frontEndSkillMap.forEach((value: string, key: string) => {
+      const container = document.getElementById(key);
+      container.innerHTML = '<div class="popUp-skill-title"> ' + key + ' </div>';
+
+      for (let i = 0; i < parseInt(value, 10); i++) {
+        container.innerHTML += this.starHtml;
+      }
+      container.innerHTML += ' (' + value + ')';
+    });
+  }
+
+  populateBackEndDialog() {
+    this.backEndSkillMap.forEach((value: string, key: string) => {
+      const container = document.getElementById(key);
+      container.innerHTML = '<div class="popUp-skill-title"> ' + key + ' </div>';
+
+      for (let i = 0; i < parseInt(value, 10); i++) {
+        container.innerHTML += this.starHtml;
+      }
+      container.innerHTML += ' (' + value + ')';
+    });
+  }
+
+  populateIndustryDialog() {
+    this.industrySkillMap.forEach((value: string, key: string) => {
+      const container = document.getElementById(key);
+      container.innerHTML = '<div class="popUp-skill-title"> ' + key + ' </div>';
+
+      for (let i = 0; i < parseInt(value, 10); i++) {
+        container.innerHTML += this.starHtml;
+      }
+      container.innerHTML += ' (' + value + ')';
+    });
   }
 
   showFrontEndDialog() { this.frontEndDialog = true; }
