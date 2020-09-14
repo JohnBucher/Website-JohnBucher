4018f5cfd55918e33bdf9b29dc891aab002a3431
JohnBucher <johnbucher289@gmail.com>
2020-09-13 18:14:06 -0600

    Pushed improvements to 'Background' section including improvements to the text and fixing the divider's color to match the JB Logo

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -7,11 +7,15 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class BackgroundComponent {
-  backgroundBlurb = `Software Engineer specializing in Web Development and Web Service technologies.
-                      I graduated with a bachelor’s degree in Computer Science from Shippensburg University and have plans to continue my
-                      education with a master’s degree in the very near future. I am currently working for Lockheed Martin as an
-                      Asc Software Engineer in the Space department. I am currently focused on learning the industry and
-                      developing as an early-career professional.`;
+  backgroundBlurb = `My name is John Bucher and I am a Software Engineer specializing in Full Stack Web Development and Web Service
+   technologies. This website was created to serve as a consolidation and showcase of my projects, skills, education,
+   and experience!<br /><br />
+   I graduated from Shippensburg University in the Spring of 2018 with a Bachelor of Science in Computer Science.
+   I graduated Cum Laude and as a member of the Shippensburg Honor’s Program. I plan to continue my education in Computer
+   Science with a master’s degree in the very near future.<br /><br />
+   I am currently working for Lockheed Martin as a Software Engineer in the Space Department. My main duties involve
+   developing and maintaining web-based applications using frameworks and languages such as Angular, Spring, and SQL among others.
+  `;
 
   constructor() {
   }
