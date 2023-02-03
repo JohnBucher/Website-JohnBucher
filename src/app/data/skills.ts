8b693f4a9102187200b48d37282249a2593c692e
JohnBucher <johnbuchercs@gmail.com>
2023-02-02 23:09:26 -0700

    Changes to skills and groups

--- src/app/data/skills.ts
+++ src/app/data/skills.ts
@@ -4,41 +4,41 @@ export const skillList = [
     {
         icon: 'fab fa-angular',
         name: 'Front End Technologies',
-        skills: ['Angular/Typescript', 'AngularJS', 'Javascript', 'ASP.NET', 'Ruby on Rails']
-    },
-    {
-        icon: 'fas fa-desktop',
-        name: 'Web Design',
-        skills: ['HTML', 'CSS', 'Bootstrap']
+        skills: ['Angular', 'React', 'AngularJS', 'ASP.NET', 'Bootstrap']
     },
     {
         icon: 'fab fa-node-js',
         name: 'Back End Technologies',
-        skills: ['Java/Spring', 'Node.js', 'Kotlin']
+        skills: ['Spring Framework', 'Node.js']
+    },
+    {
+        icon: 'fas fa-desktop',
+        name: 'Web Development',
+        skills: ['Ruby on Rails', 'HTML', 'CSS', 'SCSS', 'JSON', 'XML']
+    },
+    {
+        icon: 'fas fa-gears',
+        name: 'DevSecOps',
+        skills: ['AWS', 'logstash', 'GitHub Pages', 'GitHub Actions (CI/CD)']
     },
     {
         icon: 'fas fa-database',
         name: 'Databases',
-        skills: ['SQL', 'SQL Server']
+        skills: ['Elasticsearch', 'SQL', 'SQL Server']
+    },
+    {
+        icon: 'fas fa-code',
+        name: 'Languages',
+        skills: ['Typescript', 'Javascript', 'Java', 'C#', 'C', 'Ruby', 'PHP']
     },
     {
         icon: 'fas fa-tools',
-        name: 'Industry Tools',
+        name: 'Development Tools',
         skills: ['Git', 'VS Code', 'Visual Studio', 'IntelliJ', 'Eclipse']
     },
     {
         icon: 'fas fa-briefcase',
-        name: 'Industry Standards',
-        skills: ['Agile', 'Continuous Self-Learning']
-    },
-    {
-        icon: 'fas fa-code',
-        name: 'Other Languages',
-        skills: ['C#', 'C', 'Python', 'JSON', 'XML', 'PHP']
-    },
-    {
-        icon: 'fas fa-project-diagram',
-        name: 'Miscellaneous',
-        skills: ['Computer Building', 'Photo/Video Editing']
+        name: 'Industry Tools',
+        skills: ['Scrum Master', 'Agile', 'Stakeholder Management', 'Continuous Self-Learning']
     }
 ] as SkillGroup[];
