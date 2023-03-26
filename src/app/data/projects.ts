0262ebb241647e332fa95de4eb1deabf012fb292
JohnBucher <johnbucher289@gmail.com>
2023-03-26 11:28:09 -0600

    Adding new React app (TENZI) to projects section

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -3,14 +3,15 @@ import { Project } from "../models/project-model";
 export const projects = [
     {
       name: 'Johnbucher.me',
-      id: '2',
+      id: '1',
       image: './assets/Projects/Projects-John_Website.PNG',
       tags: [
-        { name: 'Angular (15)', icon: 'fas fa-desktop', color: '#c3002f' },
+        { name: 'Angular (15)', icon: 'fab fa-angular', color: '#c3002f' },
         { name: 'Typescript', icon: 'fas fa-code', color: '#2f74c0' },
         { name: 'SASS', icon: 'fas fa-paintbrush', color: '#bf4080' },
         { name: 'Bootstrap', icon: 'fas fa-paintbrush', color: '#702cf6' },
         { name: 'CI/CD (GitHub Actions)', icon: 'fas fa-gears', color: '#3fb950' },
+        { name: 'ESLint', icon: 'fa-solid fa-list', color: '#482fbd' },
         { name: 'Karma', icon: 'fas fa-flask-vial', color: '#369b8b' },
         { name: 'Jasmine', icon: 'fas fa-flask-vial', color: '#8a4182' }
       ],
@@ -19,12 +20,35 @@ export const projects = [
         concepts and design practices.`,
       links: [
         { icon: 'fab fa-github', url: 'https://github.com/JohnBucher/Website-JohnBucher', tooltip: 'GitHub Repository' },
-        { icon: 'fas fa-external-link-alt', url: 'https://johnbucher.me/', tooltip: 'Link to Project' }
+        { icon: 'fas fa-external-link-alt', url: 'https://johnbucher.me/', tooltip: 'Link to Live Project' }
+      ]
+    },
+    {
+      name: 'TENZI',
+      id: '2',
+      image: './assets/Projects/Projects-TENZI.PNG',
+      tags: [
+        { name: 'React (18)', icon: 'fab fa-react', color: '#1487cb' },
+        { name: 'Javascript', icon: 'fas fa-code', color: '#dbc60d' },
+        { name: 'CI/CD (GitHub Actions)', icon: 'fas fa-gears', color: '#3fb950' },
+        { name: 'ESLint', icon: 'fa-solid fa-list', color: '#482fbd' },
+        { name: 'React Testing Library', icon: 'fas fa-flask-vial', color: '#15c213' },
+        { name: 'Jest', icon: 'fas fa-flask-vial', color: '#15c213' }
+      ],
+      description:
+        `Initially created alongside <a href="https://www.youtube.com/watch?v=bMknfKXIFA8">freeCodeCamp's React Tutorial</a>
+        to gain experience creating and developing React applications.
+        <br />After tutorial completion, project was then augmented with additional features such as:
+        <br />Improving the dice look-and-feel, high-score saving and reporting, CI/CD Pipeline through GitHub Actions,
+        Linting and Testing capabilities, and served on GitHub Pages`,
+      links: [
+        { icon: 'fab fa-github', url: 'https://github.com/JohnBucher/react-tenzi-game', tooltip: 'GitHub Repository' },
+        { icon: 'fas fa-external-link-alt', url: 'https://johnbucher.github.io/react-tenzi-game/', tooltip: 'Link to Live Project' }
       ]
     },
     {
       name: 'E-Commerce Mock Storefront',
-      id: '1',
+      id: '3',
       image: './assets/Projects/Projects-CompAid.PNG',
       tags: [
         { name: '.NET Framework', icon: 'fas fa-cubes', color: '#1487cb' },
@@ -33,7 +57,7 @@ export const projects = [
         { name: 'C#', icon: 'fas fa-code', color: '#1d9924' },
         { name: 'Bootstrap', icon: 'fas fa-paintbrush', color: '#702cf6' },
         { name: 'Relational Databases', icon: 'fas fa-database', color: '#4479a1' },
-        { name: 'SQL Server', icon: 'fas fa-magnifying-glass', color: '#e62e27' },
+        { name: 'SQL Server', icon: 'fas fa-magnifying-glass', color: '#e62e27' }
       ],
       description:
         `A Mock E-Commerce Storefront made as a part of my 2017 Summer Internship at Computer Aid, Inc.
